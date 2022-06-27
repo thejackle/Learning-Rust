45fb90b60212a1f9f07d29623116bb76ab4acc7d
thejackle <jpavan@hotmail.ca>
2022-06-27 19:55:45 -0400

    created guesser function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 
-fn main() {
+fn Number_Guesser() {
 
     println!("Guess the number!");
     println!("Input your guess now.");
@@ -9,7 +9,12 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess);
+        .read_line(&mut guess)
+        .expect("Could not get answer");
 
     println!("You guessed: {}", guess)
+}
+
+fn main() {
+    Number_Guesser();
 }
