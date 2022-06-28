7cded908ebc11d8ff55dd2bde1dabf5e5ac45de1
thejackle <jpavan@hotmail.ca>
2022-06-27 21:23:14 -0400

    add primatives function for variables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io;
 
 
-fn Number_Guesser() {
+fn number_guesser() {
 
+    // a number guessing game
     println!("Guess the number!");
     println!("Input your guess now.");
 
@@ -13,8 +14,26 @@ fn Number_Guesser() {
         .expect("Could not get answer");
 
     println!("You guessed: {}", guess)
+
+    // TODO create random number to compare to
+}
+
+fn primatives(){
+    // lets look at some variables
+    // Variables are declared with let
+    let _new_variable: i32;
+
+    // Variables are immutaqble by default but can be changed to mutable with mut
+    let mut _new_mutable_var = 22;
+    _new_mutable_var = 50;
+
+    // bool variables
+    let _new_bool = true;
+    // with annotation
+    let _new_bool_two: bool = false;
+
 }
 
 fn main() {
-    Number_Guesser();
+    primatives();
 }
